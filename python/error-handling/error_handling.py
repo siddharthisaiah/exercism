d17bb03d7606adc9de19621ccfdee13caf9c066c
Siddharth <siddharthisaiah@gmail.com>
2019-05-16 23:46:41 +0530

    user 'with' to open file like object for error-handling

--- python/error-handling/error_handling.py
+++ python/error-handling/error_handling.py
@@ -17,8 +17,6 @@ def handle_error_by_returning_tuple(input_data):
 
 
 def filelike_objects_are_closed_on_exception(filelike_object):
-    try:
-        filelike_object.open()
-        filelike_object.do_something()
-    finally:
-        filelike_object.close()
+    with filelike_object as fo:
+        fo.do_something()
+        
