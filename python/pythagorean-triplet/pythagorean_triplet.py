3bf0229df4e3947f5bf804ef84885fad07d9bc0b
Siddharth <siddharthisaiah@gmail.com>
2019-05-24 23:34:08 +0530

    solved pythagorean tripletes O(n^3) solution

---
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -0,0 +1,14 @@
+import math
+
+def triplets_with_sum(sum_of_triplet):
+    triplets = set()
+    
+    for a in range(2, int(sum_of_triplet / 3) + 1):
+        for b in range(a+1, int(sum_of_triplet/2)+1):
+            
+            c = sum_of_triplet - a - b
+            
+            if pow(a,2) + pow(b,2)  == pow(c, 2):
+                triplets.add(tuple(sorted((a, b, int(c)))))
+
+    return triplets
