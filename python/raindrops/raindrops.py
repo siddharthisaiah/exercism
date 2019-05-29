423c498e2e03efc096ece6cf21fb0bb7cecd2b5d
Siddharth <siddharthisaiah@gmail.com>
2019-05-29 23:51:47 +0530

    remove get_factors method and use list comprehension

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,20 +1,5 @@
 def raindrops(number):
-    factors = get_factors(number)
     raindrops = [(3,"Pling"), (5, "Plang"), (7, "Plong")]
-    raindrop_speak = ""
-
-    for raindrop in raindrops:
-        if raindrop[0] in factors:
-            raindrop_speak += raindrop[1]
-
-    return str(number) if not raindrop_speak else raindrop_speak
-
-def get_factors(n):
-    # assumes n > 1
-    facs = {1,n}
-
-    for x in range(2, (n // 2) + 1):
-        if n % x == 0:
-            facs.add(x)
-            
-    return facs
+    raindrop_speak = [raindrop[1] for raindrop in raindrops if number % raindrop[0] == 0]
+    
+    return str(number) if not raindrop_speak else "".join(raindrop_speak)
