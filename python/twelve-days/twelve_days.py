c1811138b146bf3d19e01a566f83fba09c733dac
Siddharth <siddharthisaiah@gmail.com>
2019-06-13 12:45:12 +0530

    completed twelve days of christmas with f'strings and if assignment expression

---
+++ python/twelve-days/twelve_days.py
@@ -0,0 +1,25 @@
+def recite(start_verse, end_verse):
+    verses = [0, 'first', 'second', 'third', 'fourth',
+              'fifth', 'sixth', 'seventh', 'eighth',
+              'ninth', 'tenth', 'eleventh', 'twelfth']
+
+    gifts = ["twelve Drummers Drumming",
+             "eleven Pipers Piping", 
+             "ten Lords-a-Leaping",
+             "nine Ladies Dancing",
+             "eight Maids-a-Milking",
+             "seven Swans-a-Swimming",
+             "six Geese-a-Laying",
+             "five Gold Rings",
+             "four Calling Birds",
+             "three French Hens",
+             "two Turtle Doves",
+             "and a Partridge in a Pear Tree"]
+    
+    song = []
+    
+    for v in range(start_verse, end_verse + 1):
+        v_gifts = gifts[-v][4:] if v == 1 else ', '.join(gifts[-v:]) # gifts dont include the word 'and ' if its the first verse
+        song.append(f"On the {verses[v]} day of Christmas my true love gave to me: {v_gifts}.")
+        
+    return song
